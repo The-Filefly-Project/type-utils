66af55f7ad8d6a7b60f4ff689a7e96d7f8f10558
4Sight <116753476+4S1ght@users.noreply.github.com>
2024-03-12 14:37:27 +0100

    [add] LogEventCapable type guard

--- types/logging.d.ts
+++ types/logging.d.ts
@@ -1,4 +1,15 @@
+import type EventEmitter from 'events'
+
 /**
  * Logging levels used across the Filefly server.
  */
 declare type LogLevel = 'critical' | 'error' | 'warn' | 'notice' | 'info' | 'http' | 'debug'
+
+/**
+ * Type guard used to make sure filefly libraries comply with the established logging
+ * scheme used in the project and that they can safely emit log events.
+ * 
+ * Any library that has the necessity or is of the high enough importance to the
+ * security of the server implements this interface.
+ */
+declare type LogEventCapable<LogType = any> = EventEmitter<Record<LogLevel, LogType[]>>
