6f12518e70ef646194e2348a56db28bdde534ba8
4Sight <116753476+4S1ght@users.noreply.github.com>
2024-03-12 14:16:01 +0100

    [add] LogLevel type for use globally across filefly libraries

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,2 +1,5 @@
 //  Function return types
 /// <reference path="./returns.d.ts"/>
+
+//  Logging
+/// <reference path="./logging.d.ts"/>
---
+++ types/logging.d.ts
@@ -0,0 +1,4 @@
+/**
+ * Logging levels used across the Filefly server.
+ */
+declare type LogLevel = 'critical' | 'error' | 'warn' | 'notice' | 'info' | 'http' | 'debug'
